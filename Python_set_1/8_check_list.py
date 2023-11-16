ea5aea6e4ef3587dbca18f35a3a402c1d30e89bc
ShubhangiKumbhar28 <117151339+ShubhangiKumbhar28@users.noreply.github.com>
2023-11-16 22:40:14 +0530

    Add files via upload

---
+++ Python_set_1/8_check_list.py
@@ -0,0 +1,10 @@
+## 8. Write a Python program to check a list is empty or not. 
+
+l = ['A','B',5,9,2,5,1]
+l1=[]
+
+if len(l)==0:
+    print("list is empty.")
+else:
+    print("list is not empty.")
+
